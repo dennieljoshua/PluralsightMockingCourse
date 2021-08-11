307c52d6b05c078593a325730066f149b666facd
Jackbard <fcktardjoe@gmail.com>
2021-08-11 13:21:07 +0800

    Added a new project for testing with xUnit

---
+++ PluralsightMockingCourse.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PluralsightMockingCourse.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
